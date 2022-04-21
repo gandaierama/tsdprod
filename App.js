f2682e0b2774e50349bcde81deb3acb7ca03b6da
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2022-04-21 19:59:55 -0300

    init

--- App.js
+++ App.js
@@ -7,6 +7,39 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import * as Location from "expo-location";
 import MapView, { Marker } from "react-native-maps";
 import OneSignal from "react-native-onesignal";
+import {
+  Pressable,
+  Vibration,
+  ScrollView,
+  Image,
+  PermissionsAndroid,
+  Dimensions,
+  SafeAreaView,
+  Alert,
+  TextInput,
+  ImageBackground,
+  StyleSheet,
+  View,
+  AsyncStorage,
+  Animated,
+  AppState,
+  Share,
+  ToastAndroid
+} from "react-native";
+
+import {
+  Divider,
+  Input,
+  Card,
+  Dialog,
+  ListItem,
+  Header as HeaderRNE,
+  HeaderProps,
+  Icon,
+  Button,
+  Text,
+  Switch
+} from "react-native-elements";
 
 export default function App() {
   return (
