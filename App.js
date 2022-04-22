c5a3b34aa7fbed95045ba51406170734eba713e0
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2022-04-22 07:41:19 -0300

    share

--- App.js
+++ App.js
@@ -41,6 +41,38 @@ import {
   Switch
 } from "react-native-elements";
 
+
+const Stack = createNativeStackNavigator();
+const baseUrl = "http://api.tsdmotoboys.com.br/";
+
+const ShareBase = () => {
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message:
+          'React Native | A framework for building native apps using React',
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+  return (
+    <View style={{ marginTop: 50 }}>
+      <Button onPress={onShare} title="Share" />
+    </View>
+  );
+};
+
+
 export default function App() {
   return (
     <View style={styles.container}>
